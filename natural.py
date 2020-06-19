cd2384d6654223c235e4b1c12b2cea66b1ca6bfc
Frank Bryce <jonnyjack7@gmail.com>
2020-06-18 22:02:03 -0400

    Change syntax of rules to use named capture groups instead of indices.
    This will support having rules with multiple theorem inputs. This is
    required to encode more complicated logical offerings for the formal
    systems I'd like to make.

---
+++ natural.py
@@ -0,0 +1,50 @@
+import re
+
+def _isAxiom1(a):
+    return a in A
+
+def AddAxiom(a):
+    if _isAxiom1(a):
+        VerifyTheorem(a)
+        A.add(a)
+        T.add(a)
+    else:
+        raise Exception("this is not an axiom")
+
+def AddTheorem(r, t):
+    assert t in T
+    assert r in R
+    ms = re.match(r[0], t)
+    if not ms:
+        return
+    newT = r[-1]
+    for name, m in ms.groupdict().items():
+        newT = re.sub(r'\?<{}>'.format(name), m, newT)
+    VerifyTheorem(newT)
+    T.add(newT)
+
+# verify that no illegal characters are in theorems
+def VerifyTheorem(t):
+    # required for encoding named capture groups.
+    assert '(' not in t
+    assert 'P' not in t
+    assert '?' not in t
+    assert '<' not in t
+    assert '>' not in t
+    assert ')' not in t
+    # required for encoding non-capturing group
+    assert ':' not in t
+
+
+A = set(['N-'])
+T = A | set([
+])
+for t in T:
+    VerifyTheorem(T)
+R = set([
+    (r"N(?P<x>-+)", r"N?<x>-"),
+    (r"N(?P<x>-+)", r"?<x>A?<x>A?<x>"),
+    (r"(?P<x>-+)A(?P<y>-+)A-(?P<z>-+)", r"?<x>?<y>A?<y>A?<z>"),
+    (r"(?P<x>-+)A-+A-", r"S?<x>"),
+])
+
